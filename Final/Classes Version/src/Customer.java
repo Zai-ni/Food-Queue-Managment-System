d01d02b3132a9b806c22d2c491ec33d41f4bb4d1
Athiyya Zaini <athiyyazaini2001@gmail.com>
2023-08-13 14:00:44 +0530

    Added view queue customer name feature Added change queue customer details

--- Final/Classes Version/src/Customer.java
+++ Final/Classes Version/src/Customer.java
@@ -9,6 +9,18 @@ public class Customer {
 
     }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public void setNoOfBurgers(int noOfBurgers) {
+        this.noOfBurgers = noOfBurgers;
+    }
+
     public String getFirstName(){
         return firstName;
     }
